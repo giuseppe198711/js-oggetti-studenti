44d28e81d082c5d80b50767f9b08f586c118a415
Giuseppe <giuseppedf8711@gmail.com>
2020-09-15 22:51:29 +0200

    finito senza Handlebars

--- script.js
+++ script.js
@@ -1,49 +1,82 @@
-// $(document).ready(function() {
-
    // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
    // Stampare a schermo attraverso il for in tutte le proprietà.
+
    // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti
    // e stampare per ognuno nome e cognome
+
    // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto
    // studente inserendo nell’ordine: nome, cognome e età.
    // Concentratevi prima sulla logica stampando il risultato in console, poi quando avete
    // terminato, provate ad utilizzare il template engine Handlebars e a stampare i risultati
    // direttamente nel documento HTML.
 
+
+
+   // creo l oggetto pagella e lo descrivo con nome cognome e età
+   var pagella = {
+     "cognome": "De Felice",
+     "nome": "Giuseppe",
+     "eta": "33",
+     "italiano": "7",
+     "matematica": "7",
+     "inglese": "7",
+   };
+
+   // attaraverso il ciclo for stampo a schermo l elemento pagella in tutte le sue proprietà
+   for (var key in pagella) {
+    console.log(key + ":" + pagella[key]);
+   }
+
+  // creo un array di oggetti di studenti (pagelle) lo faccio ciclare su tutti gli studenti e faccio
+  // il console log di solo di nome e cognome
   var pagelle = [
   {
-    "Cognome": "De Felice",
-    "Nome": "Giuseppe",
-    "Età": "20",
-    "italiano": "8",
+    "cognome": "De Felice",
+    "nome": "Giuseppe",
+    "eta": "33",
+    "italiano": "7",
     "matematica": "7",
-    "inglese": "6",
-  },
-
-  {
-    "Cognome": "Filone",
-    "Nome": "Stefano",
-    "Età": "20",
-    "italiano": "9",
-    "matematica": "8",
     "inglese": "7",
   },
 
   {
-    "Cognome": "Peca",
-    "Nome": "Francesco",
-    "Età": "20",
-    "italiano": "7",
-    "matematica": "9",
+    "cognome": "Filone",
+    "nome": "Stefano",
+    "eta": "24",
+    "italiano": "9",
+    "matematica": "7",
     "inglese": "8",
+  },
+
+  {
+    "cognome": "Peca",
+    "nome": "Francesco",
+    "eta": "26",
+    "italiano": "5",
+    "matematica": "8",
+    "inglese": "9",
   }
-
-
  ];
 
   for (var i = 0; i < pagelle.length; i++) {
-    console.log(pagelle[i]);
+
     for (var key in pagelle[i]) {
-      console.log(key + " : " +pagelle[i][key]);
+      if (key == "cognome" || key == "nome" ) {
+        console.log(pagelle[i][key]);
+      }
     }
   }
+
+  // chiedo allo studente di inserire il suon nome il suo cognome e la sua eta e lo vado a inserire
+  // nelle variabili
+  var nomeUtente = prompt("inserisci il tuo nome");
+  var cognomeUtente = prompt("inserisci il tuo cognome");
+  var etaUtente = parseInt(prompt("inserisci la tua eta"));
+
+  var utente = {
+    "nome": nomeUtente,
+    "cognome": cognomeUtente,
+    "eta" : etaUtente,
+  }
+
+  console.log(utente);
